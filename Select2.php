4b74ff2657cc50451a5d46b121ddbe640efbb6e9
Kostas Thermos <kthermos@lely.com>
2015-06-09 12:57:36 +0200

    Language and compatibility with select2 4.0.0

--- Select2.php
+++ Select2.php
@@ -26,7 +26,7 @@ class Select2 extends CInputWidget
 
     /** @var array See CHtml::listData() */
     public $data;
-    public $onTrigger=array();
+    public $onTrigger = array();
 
     /** Initiialize method to publish assets */
     public function init()
@@ -39,7 +39,7 @@ class Select2 extends CInputWidget
         } elseif (isset($this->htmlOptions['multiple'])) {
             $this->multiple = true;
         }
-        
+
         if (isset($this->htmlOptions['placeholder'])) {
             $this->settings['placeholder'] = $this->htmlOptions['placeholder'];
         } elseif (isset($this->htmlOptions['data-placeholder'])) {
@@ -47,21 +47,22 @@ class Select2 extends CInputWidget
         }
 
         if (isset($this->htmlOptions['select2Options'])) {
-            if(array_key_exists("onTrigger",$this->htmlOptions['select2Options'])){
-                    $this->onTrigger    =   CMap::mergeArray($this->onTrigger, $this->htmlOptions['select2Options']['onTrigger']);
-                    unset($this->htmlOptions['select2Options']['onTrigger']);
+            if (array_key_exists("onTrigger", $this->htmlOptions['select2Options'])) {
+                $this->onTrigger = CMap::mergeArray($this->onTrigger,
+                    $this->htmlOptions['select2Options']['onTrigger']);
+                unset($this->htmlOptions['select2Options']['onTrigger']);
             }
             $this->settings = CMap::mergeArray($this->settings, $this->htmlOptions['select2Options']);
             unset($this->htmlOptions['select2Options']);
         }
-        
+
     }
 
     /**Update scripts and bind events */
     public function run()
     {
         list($name, $id) = $this->resolveNameID();
-        
+
         if (isset($this->htmlOptions['id'])) {
             $id = $this->htmlOptions['id'];
         } else {
@@ -71,19 +72,11 @@ class Select2 extends CInputWidget
         if (isset($this->htmlOptions['name'])) {
             $name = $this->htmlOptions['name'];
         }
-        
-        if (isset($this->settings['ajax'])) {
-            if (isset($this->model)) {
-                echo CHtml::textField($name, $this->model->{$this->attribute}, $this->htmlOptions);
-            } else {
-                echo CHtml::textField($name, $this->value, $this->htmlOptions);
-            }
+
+        if (isset($this->model)) {
+            echo CHtml::dropDownList($name, $this->model->{$this->attribute}, $this->data, $this->htmlOptions);
         } else {
-            if (isset($this->model)) {
-                echo CHtml::dropDownList($name, $this->model->{$this->attribute}, $this->data, $this->htmlOptions);
-            } else {
-                echo CHtml::dropDownList($name, $this->value, $this->data, $this->htmlOptions);
-            }
+            echo CHtml::dropDownList($name, $this->value, $this->data, $this->htmlOptions);
         }
 
         $this->registerScripts($id);
@@ -97,29 +90,27 @@ class Select2 extends CInputWidget
         $cs->registerCssFile($this->assetsDir . '/select2.min.css');
         $cs->registerScriptFile($this->assetsDir . '/select2.full.min.js');
 
-        $lang = strtoupper(str_replace('_', '-', Yii::app()->language));
-        $lang[0] = strtolower($lang[0]);
-        $lang[1] = strtolower($lang[1]);
+        $lang = substr(str_replace('_', '-', strtolower(Yii::app()->language)), 0, 2);
 
         $cs->registerScriptFile($this->assetsDir . '/i18n/' . $lang . '.js');
-        
+
         $settings = CJavaScript::encode($this->settings);
-        
-        
-        $onTriggers='';
-        if(!empty($this->onTrigger)){
-            foreach($this->onTrigger as $k=>$v){
-                $onTriggers .='.on(\''.$k.'\','.CJavaScript::encode($v).')';
+
+
+        $onTriggers = '';
+        if (!empty($this->onTrigger)) {
+            foreach ($this->onTrigger as $k => $v) {
+                $onTriggers .= '.on(\'' . $k . '\',' . CJavaScript::encode($v) . ')';
             }
         }
-        
+
         $cs->registerScript("{$id}_select2", ""
-                . "$('#{$id}').select2({$settings})".$onTriggers.";"
-            );
-            
+            . "$('#{$id}').select2({$settings})" . $onTriggers . ";"
+        );
+
     }
 
-    
+
     public static function dropDownList($name, $select, $data, $htmlOptions = array())
     {
         return Yii::app()->getController()->widget(__CLASS__, array(
@@ -132,7 +123,8 @@ class Select2 extends CInputWidget
 
     public static function activeDropDownList($model, $attribute, $data, $htmlOptions = array())
     {
-        return self::dropDownList(CHtml::activeName($model, $attribute), CHtml::value($model, $attribute), $data, $htmlOptions);
+        return self::dropDownList(CHtml::activeName($model, $attribute), CHtml::value($model, $attribute), $data,
+            $htmlOptions);
     }
 
     /** Multiple elect */
@@ -149,7 +141,8 @@ class Select2 extends CInputWidget
 
     public static function activeMultiSelect($model, $attribute, $data, $htmlOptions = array())
     {
-        return self::multiSelect(CHtml::activeName($model, $attribute) . '[]', CHtml::value($model, $attribute), $data, $htmlOptions);
+        return self::multiSelect(CHtml::activeName($model, $attribute) . '[]', CHtml::value($model, $attribute), $data,
+            $htmlOptions);
     }
 
 }
